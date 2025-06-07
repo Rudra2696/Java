defa8ced955efd447e8c347d356524d5b271dff3
Rudra Patel <rhpatel1023@gmail.com>
2025-06-07 19:09:20 +0530

    can do addotion up to n terms

---
+++ ex2.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+
+public class ex2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the nth term : ");
+        int n = sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum=sum+i;
+        }
+        System.out.println("\n The summation of "+n+"th term = "+sum);
+    }
+}
