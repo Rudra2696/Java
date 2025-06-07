700fe965c11904023fbe236aaeede6cbfe896833
Rudra Patel <rhpatel1023@gmail.com>
2025-06-07 18:39:47 +0530

    print 0 to 10 using do while loop

---
+++ dwloop.java
@@ -0,0 +1,11 @@
+
+public class dwloop {
+    public static void main(String[] args) {
+        int c=0;
+        do { 
+            System.out.println(c);
+            c++;
+        } while (c<=10);
+    }
+    
+}
