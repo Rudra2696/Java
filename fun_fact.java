da9f55365260f9619ee3d17800bb9adcebac212c
Rudra Patel <rhpatel1023@gmail.com>
2025-06-08 13:50:19 +0530

    fixed bug

--- fun_fact.java
+++ fun_fact.java
@@ -5,18 +5,25 @@ import java.util.Scanner;
 
 public class fun_fact {
 
-    public static int fact(int a) {
-        int mul=1;
-        for(int i=1;i<=a;i++){
-            mul=mul*i;
+    public static void fact(int a) {
+        if(a==0){
+            System.out.println("The factorial of 0 = 0");;
         }
-        return mul;
+        else if(a<0){
+            System.out.println("You entered negative integer");
+        }
+        else {
+            int mul=1;
+            for(int i=1;i<=a;i++){
+                mul=mul*i;
+            }
+        }    
     }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter any integer : ");
         int a=sc.nextInt();
-        System.out.println("The Factorial of "+a+" = "+fact(a));
+        fact(a);
     }
 }
