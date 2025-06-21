8e19d12b39ec21a54bb3d12a0c76adfe360d52bf
Rudra Patel <rhpatel1023@gmail.com>
2025-06-21 13:45:41 +0530

    calculate the total possible path and then print it

---
+++ path_calculater.java
@@ -0,0 +1,19 @@
+public class path_calculater {
+    public static int path(int i,int j,int n,int m){
+       if(i==n || j==m){
+        return 0;
+       }
+       if(i==n-1 && j==m-1){
+        return 1;
+       }
+        int down=path(i+1,j,n,m);
+        int right=path(i,j+1,n,m);
+        return down+right;
+    }
+    public static void main(String[] args) {
+        int n=3;
+        int m=4;
+        int p=path(0, 0, n, m);
+        System.out.println(p);
+    }
+}
